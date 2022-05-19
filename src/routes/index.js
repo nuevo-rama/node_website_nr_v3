76e8bca678f0b4fab0317400c8a3d86e4241bfb7
nuevo-rama <ramiromelero@gmail.com>
2022-05-19 19:53:33 -0300

    set up index routes

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+module.exports = router;
